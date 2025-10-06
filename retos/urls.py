d45c06dc42c35a862f65e3e048ac816d112dec1f
leandrodlf <leandrodlf35@gmail.com>
2025-10-05 23:55:19 -0300

    Update urls.py

--- retos/urls.py
+++ retos/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('login/', views.login_view, name='login'),  
     path('logout/', views.logout_view, name='logout'),
+    path('api/retos/', views.api_retos_esb, name='api_retos_esb'),
 ]
